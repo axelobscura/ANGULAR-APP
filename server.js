307d6e6c2e2bb3d69b27a45d8a23a8bdef62cd57
Axel <axosar@gmail.com>
2019-09-30 12:36:32 -0500

    Adding the express part

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const app = require('./backend/app');
 
-const server = http.createServer((req, res) => {
-  res.end('this is my first response');
-});
+//const port = process.en.PORT || 3000;
+const port = 3000;
+
+app.set('port', port);
+const server = http.createServer(app);
 
 //server.listen(process.en.PORT || 3000);
-server.listen(3000);
+server.listen(port);
