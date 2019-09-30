318313d677014053b173690c1debe4ce7dbb888b
Axel <axosar@gmail.com>
2019-09-30 15:52:31 -0500

    Adding the backen route for posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Forst middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hello from express");
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'wefwefwefdcscw',
+      title: 'First server side post',
+      message: 'This content is coming from the server.'
+    },
+    {
+      id: '46h46h54ere4',
+      title: 'Second server side post',
+      message: 'This content is coming from the server.'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
