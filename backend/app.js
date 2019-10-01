b1a12dea843bf7d1de32dec03e3d1761b9414c3a
Axel <axosar@gmail.com>
2019-10-01 16:00:22 -0500

    Getting data from the db fetching posts

--- backend/app.js
+++ backend/app.js
@@ -42,21 +42,11 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'wefwefwefdcscw',
-      title: 'First server side post',
-      message: 'This content is coming from the server.'
-    },
-    {
-      id: '46h46h54ere4',
-      title: 'Second server side post',
-      message: 'This content is coming from the server.'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched succesfully!',
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched succesfully!',
+      posts: documents
+    });
   });
 });
 
