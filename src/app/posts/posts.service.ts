b33f40ee0e8eefcbd06df728c595e07a88f887e2
Axel <axosar@gmail.com>
2019-10-09 17:13:28 -0500

    Uploading file to server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,7 +4,6 @@ import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
-import { TransitiveCompileNgModuleMetadata } from '@angular/compiler';
 import { Router } from '@angular/router';
 
 @Injectable({providedIn: 'root'})
@@ -42,13 +41,22 @@ export class PostsService {
     return this.http.get<{ _id: string, title: string, content: string }>('http://localhost:3000/api/posts/' + id);
   }
 
-  addPost(title: string, content: string) {
-    const post: Post = { id: null, title, content };
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append('title', title);
+    postData.append('content', content);
+    postData.append('image', image, title);
     this.http
-      .post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
+      .post<{message: string, postId: string}>(
+        'http://localhost:3000/api/posts',
+        postData
+      )
       .subscribe((responseData) => {
-        const id = responseData.postId;
-        post.id = id;
+        const post: Post = {
+          id: responseData.postId,
+          title: title,
+          content: content
+        };
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
         this.router.navigate(["/"]);
