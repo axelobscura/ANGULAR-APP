7261d71b250487a0ce190dad20cf3c54be7653c6
Axel <axosar@gmail.com>
2019-10-24 14:08:40 -0500

    Fix de referencia

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import { AuthData } from "./auth-data.model";
+import { AuthData } from './auth-data.model';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
 
   createUser(email: string, password: string) {
-    const authdata: AuthData = {email: email, password: password};
-    this.http.post("http://localhost:3000/api/user/signup", authdata)
+    const authdata: AuthData = {email, password};
+    this.http.post('http://localhost:3000/api/user/signup', authdata)
       .subscribe(response => {
         console.log(response);
       });
