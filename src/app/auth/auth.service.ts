9476c3bb1444e3f543dcd96cc19bb64a0ab22671
Axel <axosar@gmail.com>
2019-10-28 18:04:38 -0600

    Sending auth token to request

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,13 @@ import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private token: string;
+
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string) {
     const authdata: AuthData = {email, password};
@@ -17,9 +23,12 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authdata: AuthData = {email, password};
-    this.http.post('http://localhost:3000/api/user/login', authdata)
+    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authdata)
       .subscribe(response => {
         console.log(response);
+
+        const token = response.token;
+        this.token = token;
       });
   }
 }
