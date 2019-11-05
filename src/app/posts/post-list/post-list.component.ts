e42245766a9a55b9fece71cee5e998b4afcef78c
Axel <axosar@gmail.com>
2019-11-04 18:50:51 -0600

    Passing the user id to the frontend

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -26,14 +26,19 @@ export class PostListComponent implements OnInit, OnDestroy {
     currentPage = 1;
     pageSizeOptions = [1, 2, 5, 10];
     userIsAuthenticated = false;
+    userId: string;
     private postsSub: Subscription;
     private authStatusSub: Subscription;
 
-    constructor(public postsService: PostsService, private authService: AuthService) {}
+    constructor(
+      public postsService: PostsService,
+      private authService: AuthService
+    ) {}
 
     ngOnInit() {
       this.isLoading = true;
       this.postsService.getPosts(this.postsPerPage, this.currentPage);
+      this.userId = this.authService.getUserId();
       this.postsSub = this.postsService
         .getPostUpdateListener()
         .subscribe((postData: {posts: Post[], postCount: number}) => {
@@ -46,6 +51,7 @@ export class PostListComponent implements OnInit, OnDestroy {
         .getAuthStatusListener()
         .subscribe(isAuthenticated => {
           this.userIsAuthenticated = isAuthenticated;
+          this.userId = this.authService.getUserId();
         });
     }
 
